18607638ad151c76e4c120f51f3cdd6ddd1a6a50
LixWorth <lixworth@gmail.com>
2021-08-17 08:36:14 +0800

    wip

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,14 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!",name)
-	return message
+func Hello(name string) (string,error) {
+	if name == "" {
+		return "",errors.New("empty name")
+	}
+	message := fmt.Sprintf("Hi, %v. Welcome! \n",name)
+	return message,nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,20 @@ package main
 import (
 	"example.com/greetings"
 	"fmt"
+	"log"
 )
 
 func main() {
-	message := greetings.Hello("lixworth")
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
+
+	message1, err := greetings.Hello("rumao")
+	message, err := greetings.Hello("233")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf(message1)
 	fmt.Printf(message)
 }
